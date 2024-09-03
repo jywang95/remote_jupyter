2b54956bef0bea742ab25277d4ddd752651a22b7
jywang67 <55776671+jywang67@users.noreply.github.com>
2024-09-03 11:02:09 -0400

    remove the space

--- jupyter.sh
+++ jupyter.sh
@@ -1,8 +1,8 @@
- #!/bin/sh
- #SBATCH -A MEB128
- #SBATCH -J test
- #SBATCH -p gpu_acmhs
- #SBATCH -N 1
- #SBATCH -t 2:00:00
+#!/bin/bash
+#SBATCH -A MEB128
+#SBATCH -J test
+#SBATCH -p gpu_acmhs
+#SBATCH -N 1
+#SBATCH -t 2:00:00
 
 srun jupyter-notebook --ip=$(hostname -i) --port=9000
